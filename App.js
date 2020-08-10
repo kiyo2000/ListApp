d78f74b33ab1a5863bd276e4b29530d5d8a22667
Kiyo Kanno <kiyotakakanno2010@gmail.com>
2020-08-10 13:56:03 +1000

    Fixed with Johannes's code

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {Component} from 'react'; //Using class ->{Component}
-import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button } from 'react-native';//Add Flatlist
+import { StyleSheet, Text, View, SafeAreaView, FlatList, TextInput, Button, TouchableOpacity, TouchableWithoutFeedback  } from 'react-native';//Add Flatlist
 
 import {Item} from './components/Item'; // How to import another js file.
 
@@ -8,35 +8,47 @@ import {Item} from './components/Item'; // How to import another js file.
 export default class App extends Component {
   state ={
     expenseAmount : 0,
-    expenseCategory: ''
+    expenseCategory: '',
+    updating: false,
   }
-  ListData = [
-    { id: '1', amount: 50, category: 'food'},
-    { id: '2', amount: 150, category: 'gorceries'},
-    { id: '3', amount: 50, category: 'transport'},
-  ]
+
+  listData = []
+  // ListData = [
+  //   { id: '1', amount: 50, category: 'food'},
+  //   { id: '2', amount: 150, category: 'gorceries'},
+  //   { id: '3', amount: 50, category: 'transport'},
+  // ]
 
   render() {
     return (
-      <SafeAreaView>
-        <Text>Add your expense</Text>
-        <TextInput
-              style={styles.input}
-              placeholder="$ amount" 
-              onChangeText={ text => this.setState({ expenseAmount: parseFloat
-              (text) }) }
-              keyboardType="number-pad"/>
-        <TextInput
-          style={styles.input}
-          placeholder="category"
-          onChangeText={ text => this.setState({ expenseCategory: text }) }
-        />
-        <Button title="Add" onPress={this.addItem()} />
+      <SafeAreaView >
+        <View style={styles.main}>
+          <Text>Add your expense</Text>
+          <TextInput
+                style={styles.input}
+                placeholder="$ amount" 
+                onChangeText={ text => this.setState({ expenseAmount: parseFloat
+                (text) }) }
+                keyboardType="number-pad"/>
+          <TextInput
+            style={styles.input}
+            placeholder="category"
+            onChangeText={ text => this.setState({ expenseCategory: text }) }
+          />
+          <Button title="Add" onPress={this.addItem()} />
+        </View>
+        {/* wrap the button in view*/}
+        <View>
+          <TouchableOpacity style={styles.button} onPress={this.addItem}>
+            <Text style={styles.buttonText}>Add</Text>
+          </TouchableOpacity>
+        </View>
+
         <FlatList
-            data={this.listData}
-            renderItem={this.renderList}
-            keyExtractor={ expense => expense.id }
-            extraData={this.state.expenseAmount}
+            data = {this.listData}
+            renderItem = {this.renderList}
+            keyExtractor = { item => item.id }
+            extraData = {this.state.expenseAmount}
         />
       </SafeAreaView>
     )
@@ -45,20 +57,28 @@ export default class App extends Component {
     <Item amount={item.amount} category={item.category} />
   )
   addItem = () => {
-    if( isNaN(this.state.expenseAmount) || this.state.expenseAmount == 0){
+    if( 
+      isNaN(this.state.expenseAmount) || 
+      this.state.expenseAmount == 0 ||
+      this.state.expenseCategory == ''){
       return;
     }
     let itemId = new Data().getTime().toString()
-    let item = {
+    let listitem = {
       id: itemId,
       amount: this.state.expenseAmount,
       category: this.state.expenseCategory
     }
-    this.listData.push(item)
+    this.listData.push(listItem)
+    console.log('adding')
+    // this.setState({updating: true})
     this.setState({expenseAmount:0})
   }
 }
 
+const colors = {
+  primary : 'hsla(330, 38%, 65%, 1)'
+}
 const styles = StyleSheet.create({
   main: {
     paddingHorizontal: 10,
@@ -66,8 +86,17 @@ const styles = StyleSheet.create({
   input: {
     width:'100%',
     padding: 10,
+    borderColor: 'black',
     borderWidth: 1,
     marginVertical: 15
+  },
+  button: {
+    padding: 15,
+    backgroundColor: colors.primary
+  },
+  buttonText: {
+    color: 'white',
+    textAlign: 'center'
   }
 })
 
